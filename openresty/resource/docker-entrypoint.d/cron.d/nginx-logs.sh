d6dc9a51b79bebf808b1df9097b3cb4c5234858b
ypcui3 <ypcui3@iflytek.com>
2022-10-24 00:21:47 +0800

    优化日志切割与清理

--- openresty/resource/docker-entrypoint.d/cron.d/nginx-logs.sh
+++ openresty/resource/docker-entrypoint.d/cron.d/nginx-logs.sh
@@ -14,14 +14,28 @@ fi
 
 log_cut() { 
     cd $nginx_logs_dir
-    mmv \*.log \#1_$(date -d "1 day ago" +"%Y-%m-%d").log
+    log_yesterday_date=$(date -d "yesterday" +"%Y-%m-%d")
+    for i in $(ls *.log)
+    do
+        if [[ $(grep -E "[0-9]{4}(\-[0-9]{2}){2}" <<< ${i} | wc -l) -eq 0 ]]; then
+            if [[ ! -d ${i%.log} ]]; then
+                mkdir ${i%.log}
+            fi
+            if [[ ! -f "${i%.log}/${i%.log}_${log_yesterday_date}.log" ]]; then
+                mv ${i} ${i%.log}/${i%.log}_${log_yesterday_date}.log
+            else
+                sum=$(ls ${i%.log}/${i%.log}_${log_yesterday_date}* | wc -l)
+                mv ${i} ${i%.log}/${i%.log}_${log_yesterday_date}_${sum}.log
+            fi
+        fi
+    done
     nginx -s reopen
 }
 
 log_clean() {
-    sum=$(find ${nginx_logs_dir} -type f -mtime +${nginx_logs_retention_time} | grep -E "[0-9]{4}(\-[0-9]{2}){2}" | wc -l)
+    sum=$(find ${nginx_logs_dir} -mindepth 2 -maxdepth 2 -type f -mtime +${nginx_logs_retention_time} | grep -E "[0-9]{4}(\-[0-9]{2}){2}" | wc -l)
     if [[ ${sum} -ne 0 ]]; then
-        find ${nginx_logs_dir} -type f -mtime +${nginx_logs_retention_time} | grep -E "[0-9]{4}(\-[0-9]{2}){2}" | xargs rm
+        find ${nginx_logs_dir} -mindepth 2 -maxdepth 2 -type f -mtime +${nginx_logs_retention_time} | grep -E "[0-9]{4}(\-[0-9]{2}){2}" | xargs rm
     fi
 }
 
